713bbcb9632c515b73088e573341d60030cd4463
Ben Brunyee <brunyeeb@gmail.com>
2019-04-01 19:16:28 +0100

    added dragging

--- src/sample/DebitStuff.java
+++ src/sample/DebitStuff.java
@@ -1,12 +1,7 @@
 package sample;
 
-import javafx.beans.property.SimpleStringProperty;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.scene.control.TableView;
-
 public class DebitStuff {
     //In progress
-    public static ObservableList addDebit(String input) {
+    public static void addDebit(String input) {
     }
 }
