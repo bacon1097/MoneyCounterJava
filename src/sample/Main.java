5152dca05dc5ffb933ffe97804c81e79c9140029
Ben Brunyee <brunyeeb@gmail.com>
2019-04-02 21:43:07 +0100

    updates

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -16,6 +17,7 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         window = primaryStage;
         window.setTitle("Money Counter");
+        window.getIcons().add(new Image(getClass().getResourceAsStream("images/money-bag.png")));
         window.initStyle(StageStyle.TRANSPARENT);
         window.setOnCloseRequest(e -> {
             e.consume();
